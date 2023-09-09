5935b2af6045d0d4cb5569ea9ade2d32c9859240
8Lou <lou8@mail.ru>
2023-09-09 16:09:57 +0300

    js

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const prev = document.querySelector('.arrow__prev');
+const next = document.querySelector('.arrow__prev');
+const sliders = document.querySelectorAll('.item');
+const dots = document.querySelectorAll('.dot');
